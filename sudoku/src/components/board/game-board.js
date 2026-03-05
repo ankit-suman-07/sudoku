e6464d55ceb358716b843e7fb6077ef445278e54
Ankit Suman <ankitsuman07@gmail.com>
2026-03-05 00:42:11 -0500

    Initial test components

---
+++ sudoku/src/components/board/game-board.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Cell } from '../cell'
+
+export const GameBoard = () => {
+  return (
+    <div>
+        <Cell />
+    </div>
+  )
+}
---
+++ sudoku/src/components/cell.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Cell = () => {
+  return (
+    <div>
+        <button>Cell</button>
+    </div>
+  )
+}
